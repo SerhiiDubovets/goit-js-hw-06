2605f24165fadf72865301281127e1423b098ce2
SerhiiDubovets <Sergio.d@bigmir.net>
2022-04-18 14:27:48 +0300

    Задание 1

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+
+const categoriesAll = document.querySelectorAll('#categories li.item') 
+console.log(`Number of categories: ${categories.children.length}`)
+categoriesAll.forEach(category => {
+   console.log(`Category:`, category.firstElementChild.textContent) 
+   console.log(`Elements:`, category.lastElementChild.children.length)
+});
+
+
+
+
+
